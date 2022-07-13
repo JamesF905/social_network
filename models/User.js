eb0068acb31f839e3d093dad4a49b0929d8a3725
James <jameslfidlin@gmail.com>
2022-07-13 18:41:56 -0400

    first commit basic models setup

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address']             
+        },
+        thoughts: [ {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    });
+    userSchema.virtual('friendCount').get(function () {
+        return this.friends.length
+    });
+    
+    userSchema.pre('findOneAndDelete', { document: false, query: true }, async function() {
+        const doc = await this.model.findOne(this.getFilter());
+        console.log(doc.username);
+        await Thought.deleteMany({ username: doc.username });
+    });
+    
+    const User = model('User', userSchema);
+    module.exports = User;
